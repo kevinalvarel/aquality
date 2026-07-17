79394a90a64a80bc242afb1c5da122cda2c5e55e
kevinalvarel <muhammadkevinalvarel@gmail.com>
2026-07-17 09:33:22 +0700

    fix: update middleware matcher paths to include dynamic segments

--- middleware.ts
+++ middleware.ts
@@ -22,10 +22,10 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/explore*",
-    "/leaderboard*",
-    "/map*",
-    "/aquasisten*",
+    "/explore/:path*",
+    "/leaderboard/:path*",
+    "/map/:path*",
+    "/aquasisten/:path*",
     "/login",
     "/register",
   ],
